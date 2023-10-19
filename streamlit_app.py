d4dcc8264dc155f03a75d94d5c3299da7d580acf
likhith-snowflake <148034021+likhith-snowflake@users.noreply.github.com>
2023-10-19 16:53:05 +0530

    Update streamlit_app.py
    dine

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,4 @@
-import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')   
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avacado Toast')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['watermelon','banana'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-# Display the table on the page
-streamlit.dataframe(fruits_to_show)
 
 <Contents><Key>clothing/</Key><LastModified>2022-05-25T18:17:52.000Z</LastModified><ETag>"d41d8cd98f00b204e9800998ecf8427e"</ETag><Size>0</Size><StorageClass>STANDARD</StorageClass></Contents>
 
