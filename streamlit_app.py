e03d6941c1dfd5923da22ec3b594bc3695460535
likhith-snowflake <148034021+likhith-snowflake@users.noreply.github.com>
2023-10-19 16:52:38 +0530

    Update streamlit_app.py
    added

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header("Fruityvice Fruit Advice!")
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
-
+<Contents><Key>clothing/</Key><LastModified>2022-05-25T18:17:52.000Z</LastModified><ETag>"d41d8cd98f00b204e9800998ecf8427e"</ETag><Size>0</Size><StorageClass>STANDARD</StorageClass></Contents>
 
